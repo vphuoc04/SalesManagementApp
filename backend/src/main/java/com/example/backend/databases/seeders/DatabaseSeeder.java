b710b8c243337e9b9c1119c25be26f8e61f7d4d2
vphuoc04 <vanphuoc240904@gmail.com>
2025-01-05 18:24:47 +0700

    add migrations for seeding database

---
+++ backend/src/main/java/com/example/backend/databases/seeders/DatabaseSeeder.java
@@ -0,0 +1,52 @@
+package com.example.backend.databases.seeders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.backend.modules.admins.entities.Admin;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseSeeder.class);
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    @Override
+    public void run (String... args) throws Exception {
+        logger.info("Seeding database...");
+
+        if (isTableEmpty()) {
+            String passwordEncode = passwordEncoder.encode("password");
+            
+            Admin admin = new Admin();
+            admin.setFirstName("Phước");
+            admin.setMiddleName("Văn");
+            admin.setLastName("Đoàn");
+            admin.setEmail("vphuoc2409@gmail.com");
+            admin.setPassword(passwordEncode);
+            admin.setPhone("0123456789");
+            admin.setImage(null);
+
+            entityManager.persist(admin);
+        }
+    }
+
+    private boolean isTableEmpty() {
+        Long count = (Long) entityManager.createQuery("SELECT COUNT(id) FROM Admin").getSingleResult();
+        return count == 0;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/modules/admins/entities/Admin.java
@@ -0,0 +1,115 @@
+package com.example.backend.modules.admins.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreated() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdated() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
